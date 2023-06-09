2713b1bb61c6d6d88004d854a432c617dab9156e
josiemerson <josiemerson2013@gmail.com>
2023-06-09 16:30:18 -0300

    criado a interfase do sign UP

--- src/pages/sign-up.jsx
+++ src/pages/sign-up.jsx
@@ -1,7 +1,34 @@
+import "./sign-up.style.css";
+import imageRight from "../assets/ImagemDireita.png";
+
 export default function SignUp() {
   return (
-    <div>
-      <h1>SignUp</h1>
+    <div className="containerSignUp">
+      <div className="boxFormSignUp">
+        <div className="formSignUp">
+          <div className="formSignUpHeader">
+            <h2>Cadastre-se</h2>
+          </div>
+          <div className="formSignUpInputs">
+            <input placeholder="Nome" type="text" />
+            <input placeholder="E-mail" type="email" />
+            <input placeholder="Senha" type="password" />
+          </div>
+          <div className="formSignUpButtons">
+            <button id="register">CADASTRAR</button>
+            <button id="cancel">CANCELAR</button>
+          </div>
+        </div>
+        <div className="redirectSignIn">
+          <p>
+            Já tem cadastro? <span>Clique aqui!</span>
+          </p>
+        </div>
+      </div>
+      <div
+        className="boxImageRight"
+        style={{ backgroundImage: `url(${imageRight})` }}
+      ></div>
     </div>
   );
 }
